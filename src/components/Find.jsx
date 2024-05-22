95780b3348d206f22433472da021489b33dc0ce0
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-05-22 21:34:29 +0530

    added something

--- src/components/Find.jsx
+++ src/components/Find.jsx
@@ -14,43 +14,80 @@ const findAnime = debounce((value, previous, callback) => {
     callback(null)
     return
   }
+  callback(null)
   Anilist('find', {
     page: 1,
     perPage: 6,
     search: value,
   }, (data) => {
-    callback(data)
+    setTimeout(() => {
+      callback(data)
+    }, 500)
   })
 }, 500)
 
-const Card = ({ data, rotate, width }) => {
+const Card = ({ data, rotate, width, active }) => {
   const title =
     data.title.romaji ||
     data.title.userPreferred ||
     data.title.english ||
     data.title.native
+  console.log(title, active)
+  let bounded = rotate % 360
+  if (bounded !== 0 && bounded !== 180) {
+    if (bounded < 180) {
+      rotate += bounded * 0.1
+    } else {
+      rotate -= (360 - bounded) * 0.1
+    }
+  }
   return (
     <motion.div
+      initial={{ rotate: 0, scale: 1.0 }}
+      animate={{ rotate: rotate, scale: active ? 1.0 : 0.8 }}
+      transition={{ duration: 0.2 }}
       style={{
         transformOrigin: 'bottom center',
-        rotate: `${rotate}deg`,
+        zIndex: active ? 2 : 3,
         left: `calc(50% - ${width / 2}px)`,
+        width: width,
       }}
-      className={`h-1/2 w-[${width}px] absolute top-0 inline-block`}
+      className={`h-1/2 absolute top-0 inline-block`}
     >
       <div
         className="rounded-full p-2 w-full flex justify-end"
       >
-        <img
-          style={{ rotate: `-${rotate}deg` }}
-          className="object-cover rounded-full aspect-square w-full" src={data.coverImage.large}
-        />
+        <div className={"relative rounded-full aspect-square w-full " + (active ? '' : 'blur-[1px]')}>
+          <div className="blur absolute inset-0 rounded-full -translate-x-1 translate-y-1 bg-gradient-to-br from-teal-400 via-cyan-500 to-violet-500"></div>
+          <img
+            style={{ rotate: `-${rotate}deg` }}
+            className="absolute object-cover rounded-full h-full w-full" src={data.coverImage.large}
+          />
+        </div>
       </div>
     </motion.div >
   )
 }
+Card.propTypes = {
+  data: PropTypes.any,
+  rotate: PropTypes.number,
+  width: PropTypes.number,
+  active: PropTypes.boolean,
+}
 
 const AnimeList = ({ list }) => {
+  const [shift, setShift] = useState(0)
+  useEffect(() => {
+    if (!list || list.length === 0) {
+      return
+    }
+    const timeInterval = setInterval(() => {
+      setShift(prev => (prev + 60))
+    }, 2000)
+    return () => {
+      clearInterval(timeInterval)
+    }
+  }, [list])
   if (!list || list.length === 0) {
     return (
       <div>
@@ -58,17 +95,8 @@ const AnimeList = ({ list }) => {
       </div>
     )
   }
-  const [shift, setShift] = useState(0)
-  useEffect(() => {
-    const timeInterval = setInterval(() => {
-      setShift(prev => (prev + 60) % 360)
-    }, 2000)
-    return () => {
-      clearInterval(timeInterval)
-    }
-  }, [])
   let cards = Array(6).fill(0)
-  let side = 600, cardWidth = 200
+  let side = 600, cardWidth = 180
   return (
     <div
       style={{
@@ -80,13 +108,18 @@ const AnimeList = ({ list }) => {
       {
         cards.map((_, index) =>
           <Card
+            key={index}
             data={list[index % list.length]}
             rotate={index * 60 + shift}
             width={cardWidth}
+            active={(index * 60 + shift) % 360 === 0}
           />)
       }
     </div>
   )
+}
+AnimeList.propTypes = {
+  list: PropTypes.list
 }
 
 const Find = ({ toggleFind: closeButton }) => {
@@ -142,8 +175,8 @@ const Find = ({ toggleFind: closeButton }) => {
             />
           </motion.div>
         </div>
-        <div className="w-full mt-[100px] a-center overflow-hidden">
-          <AnimeList list={list} />
+        <div className="w-full mt-8 a-center overflow-hidden">
+          {query?.length > 0 && <AnimeList list={list} />}
         </div>
       </div>
     </motion.div>
