0dee829cce44b7bbba842daacead7368976be99d
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-06-19 23:17:16 +0530

    added backend api

---
+++ src/services/backend.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+class BackendService {
+    constructor() {
+        this.URL = 'http://127.0.0.1:5000'
+    }
+
+    get(url, params) {
+        return axios.get(this.URL + url, { params })
+    }
+
+    post(url, data) {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST',
+                'token': localStorage.getItem('token') || ''
+            }
+        }
+        return axios.post(this.URL + url, data, config)
+    }
+}
+
+const Backend = new BackendService()
+
+export default Backend
+
