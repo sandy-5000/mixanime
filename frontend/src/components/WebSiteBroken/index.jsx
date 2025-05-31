86e130cf764a009d2031495c40522cfac0338d61
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2025-05-31 08:59:13 +0530

    corrected typo

--- frontend/src/components/WebSiteBroken/index.jsx
+++ frontend/src/components/WebSiteBroken/index.jsx
@@ -5,7 +5,8 @@ const WebSiteBroken = () => {
     <div style={{ padding: 0 }} className="web-site-broken">
       <div className="ring-2 ring-teal-400 p-2 rounded-xl">
         <p className="text-slate-200 text-center text-xs">
-          The website is broken because the parent site has stopped functioning.
+          The website has stopped functioning because the parent site is no
+          longer operational.
         </p>
       </div>
     </div>,
